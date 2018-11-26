67f21c479c2b99a3e79cba1a349975ca999f1e3d
COR Students <corstudents@MacBook-Pro.localdomain>
2018-11-26 09:26:58 -0500

    tried to make a website with working api

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var userId = '603214';
 var apiKey = '865741651d7c73d7cd6988d4637a9e85';
 var data = 'JSON Request Data';
 var request = $.ajax({
-url: "https://json.astrologyapi.com/v1/"+api,
+url: "https://cors-anywhere.herokuapp.com/json.astrologyapi.com/v1/birth_details",
 method: "POST",
 dataType:'json',
 headers: {
@@ -12,8 +12,5 @@ headers: {
 data:JSON.stringify(data)
 });
 // Returns A promiss
-return( request.then( function(resp){
-return resp;
-}, function(err){
-return err;
-}));
+
+
