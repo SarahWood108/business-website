7e54ed3635cce73e12ff99659a3e34e3e201920e
COR Students <corstudents@Matts-MBP-8.localdomain>
2018-11-30 11:44:35 -0500

    tried to use API does not work

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-var userId = '603214';
-var apiKey = '865741651d7c73d7cd6988d4637a9e85';
+var userId = '';
+// the userID and API keys didn't work :(
+var apiKey = '';
 var data = 'JSON Request Data';
 var request = $.ajax({
 url: "https://cors-anywhere.herokuapp.com/json.astrologyapi.com/v1/birth_details",
